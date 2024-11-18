0fd143534c337d359aaf10c0dd3f91b788ed36d3
David Meadows <dtmeadows@stainlessapi.com>
2024-11-18 09:55:49 -0500

    chore(api): add http logging

--- modern-treasury-kotlin-client-okhttp/build.gradle.kts
+++ modern-treasury-kotlin-client-okhttp/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.25.3")
