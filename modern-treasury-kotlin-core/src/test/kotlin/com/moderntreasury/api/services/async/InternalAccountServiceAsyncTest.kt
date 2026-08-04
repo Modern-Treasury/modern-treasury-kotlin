589d56babd125a32fc3ddc9071328d9736da1b2a
mtengbot <eng.bot@moderntreasury.com>
2026-08-04 20:11:41 +0000

    seal

--- modern-treasury-kotlin-core/src/test/kotlin/com/moderntreasury/api/services/async/InternalAccountServiceAsyncTest.kt
+++ modern-treasury-kotlin-core/src/test/kotlin/com/moderntreasury/api/services/async/InternalAccountServiceAsyncTest.kt
@@ -125,6 +125,7 @@ internal class InternalAccountServiceAsyncTest {
                     )
                     .name("name")
                     .parentAccountId("parent_account_id")
+                    .status(InternalAccountUpdateParams.Status.PENDING_CLOSURE)
                     .build()
             )
 
