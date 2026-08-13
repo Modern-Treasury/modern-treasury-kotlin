0dd66ebe7b8b89abe93929078f84f153b7d2747a
mtengbot <eng.bot@moderntreasury.com>
2026-08-13 20:10:46 +0000

    seal

--- modern-treasury-kotlin-core/src/main/kotlin/com/moderntreasury/api/models/BulkResult.kt
+++ modern-treasury-kotlin-core/src/main/kotlin/com/moderntreasury/api/models/BulkResult.kt
@@ -1623,8 +1623,6 @@ private constructor(
 
             val TRANSACTION = of("transaction")
 
-            val ENTITY_LINK = of("entity_link")
-
             val TRANSACTION_LINE_ITEM = of("transaction_line_item")
 
             val BULK_ERROR = of("bulk_error")
@@ -1639,7 +1637,6 @@ private constructor(
             LEDGER_TRANSACTION,
             EXPECTED_PAYMENT,
             TRANSACTION,
-            ENTITY_LINK,
             TRANSACTION_LINE_ITEM,
             BULK_ERROR,
         }
@@ -1659,7 +1656,6 @@ private constructor(
             LEDGER_TRANSACTION,
             EXPECTED_PAYMENT,
             TRANSACTION,
-            ENTITY_LINK,
             TRANSACTION_LINE_ITEM,
             BULK_ERROR,
             /**
@@ -1682,7 +1678,6 @@ private constructor(
                 LEDGER_TRANSACTION -> Value.LEDGER_TRANSACTION
                 EXPECTED_PAYMENT -> Value.EXPECTED_PAYMENT
                 TRANSACTION -> Value.TRANSACTION
-                ENTITY_LINK -> Value.ENTITY_LINK
                 TRANSACTION_LINE_ITEM -> Value.TRANSACTION_LINE_ITEM
                 BULK_ERROR -> Value.BULK_ERROR
                 else -> Value._UNKNOWN
@@ -1704,7 +1699,6 @@ private constructor(
                 LEDGER_TRANSACTION -> Known.LEDGER_TRANSACTION
                 EXPECTED_PAYMENT -> Known.EXPECTED_PAYMENT
                 TRANSACTION -> Known.TRANSACTION
-                ENTITY_LINK -> Known.ENTITY_LINK
                 TRANSACTION_LINE_ITEM -> Known.TRANSACTION_LINE_ITEM
                 BULK_ERROR -> Known.BULK_ERROR
                 else -> throw ModernTreasuryInvalidDataException("Unknown EntityType: $value")
