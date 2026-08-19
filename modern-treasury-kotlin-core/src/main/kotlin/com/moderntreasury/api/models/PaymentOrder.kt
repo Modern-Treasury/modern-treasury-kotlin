4fa078f7ff9fdc0607ee617b229954712927c4bb
mtengbot <eng.bot@moderntreasury.com>
2026-08-19 17:00:57 +0000

    chore: regenerate SDKs from 06b508aee2550b3c6440d818d04163ef4b81594d
    Stainless-Generated-From: c09f586bbc4bd7ccbee0f8e1622ba050b6cad34d

--- modern-treasury-kotlin-core/src/main/kotlin/com/moderntreasury/api/models/PaymentOrder.kt
+++ modern-treasury-kotlin-core/src/main/kotlin/com/moderntreasury/api/models/PaymentOrder.kt
@@ -5664,8 +5664,6 @@ private constructor(
 
                 val CURRENCYCLOUD_PAYMENT_ID = of("currencycloud_payment_id")
 
-                val DC_BANK_TRANSACTION_ID = of("dc_bank_transaction_id")
-
                 val EFT_TRACE_NUMBER = of("eft_trace_number")
 
                 val EVOLVE_CORE_BATCH = of("evolve_core_batch")
@@ -5823,7 +5821,6 @@ private constructor(
                 CROSS_RIVER_TRANSACTION_ID,
                 CURRENCYCLOUD_CONVERSION_ID,
                 CURRENCYCLOUD_PAYMENT_ID,
-                DC_BANK_TRANSACTION_ID,
                 EFT_TRACE_NUMBER,
                 EVOLVE_CORE_BATCH,
                 EVOLVE_CORE_FILE_KEY,
@@ -5927,7 +5924,6 @@ private constructor(
                 CROSS_RIVER_TRANSACTION_ID,
                 CURRENCYCLOUD_CONVERSION_ID,
                 CURRENCYCLOUD_PAYMENT_ID,
-                DC_BANK_TRANSACTION_ID,
                 EFT_TRACE_NUMBER,
                 EVOLVE_CORE_BATCH,
                 EVOLVE_CORE_FILE_KEY,
@@ -6035,7 +6031,6 @@ private constructor(
                     CROSS_RIVER_TRANSACTION_ID -> Value.CROSS_RIVER_TRANSACTION_ID
                     CURRENCYCLOUD_CONVERSION_ID -> Value.CURRENCYCLOUD_CONVERSION_ID
                     CURRENCYCLOUD_PAYMENT_ID -> Value.CURRENCYCLOUD_PAYMENT_ID
-                    DC_BANK_TRANSACTION_ID -> Value.DC_BANK_TRANSACTION_ID
                     EFT_TRACE_NUMBER -> Value.EFT_TRACE_NUMBER
                     EVOLVE_CORE_BATCH -> Value.EVOLVE_CORE_BATCH
                     EVOLVE_CORE_FILE_KEY -> Value.EVOLVE_CORE_FILE_KEY
@@ -6143,7 +6138,6 @@ private constructor(
                     CROSS_RIVER_TRANSACTION_ID -> Known.CROSS_RIVER_TRANSACTION_ID
                     CURRENCYCLOUD_CONVERSION_ID -> Known.CURRENCYCLOUD_CONVERSION_ID
                     CURRENCYCLOUD_PAYMENT_ID -> Known.CURRENCYCLOUD_PAYMENT_ID
-                    DC_BANK_TRANSACTION_ID -> Known.DC_BANK_TRANSACTION_ID
                     EFT_TRACE_NUMBER -> Known.EFT_TRACE_NUMBER
                     EVOLVE_CORE_BATCH -> Known.EVOLVE_CORE_BATCH
                     EVOLVE_CORE_FILE_KEY -> Known.EVOLVE_CORE_FILE_KEY
