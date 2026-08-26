fbf424f9c015cd626356a9b2f87910736bef40e8
mtengbot <eng.bot@moderntreasury.com>
2026-08-26 20:21:11 +0000

    chore: regenerate SDKs from 3cb39bb964fa5051e3120e3e108f0eb0628d86de
    Stainless-Generated-From: 4e529571f04d0a17ba25e2221826f816e792108c

--- modern-treasury-kotlin-core/src/main/kotlin/com/moderntreasury/api/models/PaymentOrderUpdateParams.kt
+++ modern-treasury-kotlin-core/src/main/kotlin/com/moderntreasury/api/models/PaymentOrderUpdateParams.kt
@@ -209,10 +209,9 @@ private constructor(
     fun originatingPartyAddress(): OriginatingPartyAddress? = body.originatingPartyAddress()
 
     /**
-     * If present, this will replace your default company name on receiver's bank statement. For
-     * ACH, only the first 16 characters of this string will be used. This must be a name the
-     * account holder legally operates under, such as a DBA. Do not use it to originate on behalf of
-     * another person or business, including an end customer.
+     * If present, this will replace your default company name on receiver's bank statement. This
+     * field can only be used for ACH payments currently. For ACH, only the first 16 characters of
+     * this string will be used. Any additional characters will be truncated.
      *
      * @throws ModernTreasuryInvalidDataException if the JSON field has an unexpected type (e.g. if
      *   the server responded with an unexpected value).
@@ -1037,10 +1036,9 @@ private constructor(
             }
 
         /**
-         * If present, this will replace your default company name on receiver's bank statement. For
-         * ACH, only the first 16 characters of this string will be used. This must be a name the
-         * account holder legally operates under, such as a DBA. Do not use it to originate on
-         * behalf of another person or business, including an end customer.
+         * If present, this will replace your default company name on receiver's bank statement.
+         * This field can only be used for ACH payments currently. For ACH, only the first 16
+         * characters of this string will be used. Any additional characters will be truncated.
          */
         fun originatingPartyName(originatingPartyName: String?) = apply {
             body.originatingPartyName(originatingPartyName)
@@ -1879,10 +1877,9 @@ private constructor(
             originatingPartyAddress.getNullable("originating_party_address")
 
         /**
-         * If present, this will replace your default company name on receiver's bank statement. For
-         * ACH, only the first 16 characters of this string will be used. This must be a name the
-         * account holder legally operates under, such as a DBA. Do not use it to originate on
-         * behalf of another person or business, including an end customer.
+         * If present, this will replace your default company name on receiver's bank statement.
+         * This field can only be used for ACH payments currently. For ACH, only the first 16
+         * characters of this string will be used. Any additional characters will be truncated.
          *
          * @throws ModernTreasuryInvalidDataException if the JSON field has an unexpected type (e.g.
          *   if the server responded with an unexpected value).
@@ -2853,9 +2850,8 @@ private constructor(
 
             /**
              * If present, this will replace your default company name on receiver's bank statement.
-             * For ACH, only the first 16 characters of this string will be used. This must be a
-             * name the account holder legally operates under, such as a DBA. Do not use it to
-             * originate on behalf of another person or business, including an end customer.
+             * This field can only be used for ACH payments currently. For ACH, only the first 16
+             * characters of this string will be used. Any additional characters will be truncated.
              */
             fun originatingPartyName(originatingPartyName: String?) =
                 originatingPartyName(JsonField.ofNullable(originatingPartyName))
