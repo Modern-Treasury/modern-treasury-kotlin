1498c9aecdec57270117ab0e47a1ac17fdfbdfb0
mtengbot <eng.bot@moderntreasury.com>
2026-08-27 21:20:53 +0000

    Build SDK
    Stainless-Generated-From: 82c26caa40833031e189c4e41ae7bb60e49c5641

--- modern-treasury-kotlin-core/src/test/kotlin/com/moderntreasury/api/services/blocking/VirtualAccountServiceTest.kt
+++ modern-treasury-kotlin-core/src/test/kotlin/com/moderntreasury/api/services/blocking/VirtualAccountServiceTest.kt
@@ -84,6 +84,7 @@ internal class VirtualAccountServiceTest {
                             )
                             .build()
                     )
+                    .virtualAccountSettingId("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
                     .build()
             )
 
