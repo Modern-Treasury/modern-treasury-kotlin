f5c38b31e94021d04194925f1d1b2320b107e6df
mtengbot <eng.bot@moderntreasury.com>
2026-09-03 01:24:56 +0000

    seal

--- modern-treasury-kotlin-core/src/test/kotlin/com/moderntreasury/api/services/async/VirtualAccountServiceAsyncTest.kt
+++ modern-treasury-kotlin-core/src/test/kotlin/com/moderntreasury/api/services/async/VirtualAccountServiceAsyncTest.kt
@@ -65,6 +65,7 @@ internal class VirtualAccountServiceAsyncTest {
                             )
                             .build()
                     )
+                    .ledgerAccountId("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
                     .metadata(
                         VirtualAccountCreateParams.Metadata.builder()
                             .putAdditionalProperty("foo", JsonValue.from("string"))
